24eccf5308b4d06bdeeda4bee4a58bdb3832648a
Qing Zheng <qzheng@lanl.gov>
2025-07-08 15:15:15 -0600

    update for pdsw25

--- fuse_main.c
+++ fuse_main.c
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2025 Triad National Security, LLC, as operator of Los Alamos
+ * National Laboratory with the U.S. Department of Energy/National Nuclear
+ * Security Administration. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of TRIAD, Los Alamos National Laboratory, LANL, the
+ *    U.S. Government, nor the names of its contributors may be used to endorse
+ *    or promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 #define FUSE_USE_VERSION 31
 
 #include <errno.h>
@@ -15,8 +48,8 @@ struct global_state {
 	/* file descriptor for
 	 * "/dev/shm/pushdown_command" */
 	int command_fd;
-	/* file descriptor for "/dev/shm/pushdown_res" */
-	int res_fd;
+	/* file descriptor for "/dev/shm/pushdown_res[0-2]" */
+	int res_fd[3];
 };
 
 static int pseudo_getattr(const char *path, struct stat *stbuf,
@@ -35,10 +68,26 @@ static int pseudo_getattr(const char *path, struct stat *stbuf,
 			return -1 * errno;
 		}
 		stbuf->st_mode = S_IFREG | 0666;
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		int r = fstat(state->res_fd, stbuf);
+		int r = fstat(state->res_fd[0], stbuf);
+		if (r != 0) {
+			return -1 * errno;
+		}
+		stbuf->st_mode = S_IFREG | 0444;
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fstat(state->res_fd[1], stbuf);
+		if (r != 0) {
+			return -1 * errno;
+		}
+		stbuf->st_mode = S_IFREG | 0444;
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fstat(state->res_fd[2], stbuf);
 		if (r != 0) {
 			return -1 * errno;
 		}
@@ -60,10 +109,24 @@ static int pseudo_chmod(const char *path, mode_t mode,
 		if (r != 0) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		int r = fchmod(state->res_fd, mode);
+		int r = fchmod(state->res_fd[0], mode);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fchmod(state->res_fd[1], mode);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fchmod(state->res_fd[2], mode);
 		if (r != 0) {
 			return -1 * errno;
 		}
@@ -84,10 +147,24 @@ static int pseudo_chown(const char *path, uid_t uid, gid_t gid,
 		if (r != 0) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		int r = fchown(state->res_fd, uid, gid);
+		int r = fchown(state->res_fd[0], uid, gid);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fchown(state->res_fd[1], uid, gid);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fchown(state->res_fd[2], uid, gid);
 		if (r != 0) {
 			return -1 * errno;
 		}
@@ -108,10 +185,24 @@ static int pseudo_truncate(const char *path, off_t off,
 		if (r != 0) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		int r = ftruncate(state->res_fd, off);
+		int r = ftruncate(state->res_fd[0], off);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = ftruncate(state->res_fd[1], off);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = ftruncate(state->res_fd[2], off);
 		if (r != 0) {
 			return -1 * errno;
 		}
@@ -129,8 +220,18 @@ static int pseudo_open(const char *path, struct fuse_file_info *fi)
 		if (fi->fh == -1) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
-		fi->fh = open("/dev/shm/pushdown_res", fi->flags);
+	} else if (strcmp(path + 1, "res0") == 0) {
+		fi->fh = open("/dev/shm/pushdown_res0", fi->flags);
+		if (fi->fh == -1) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		fi->fh = open("/dev/shm/pushdown_res1", fi->flags);
+		if (fi->fh == -1) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		fi->fh = open("/dev/shm/pushdown_res2", fi->flags);
 		if (fi->fh == -1) {
 			return -1 * errno;
 		}
@@ -187,7 +288,9 @@ static int pseudo_flush(const char *path, struct fuse_file_info *fi)
 			char *command = "Offloader";
 			char *argument_list[] = { "Offloader",
 						  "/dev/shm/pushdown_command",
-						  "/dev/shm/pushdown_res",
+						  "/dev/shm/pushdown_res0",
+						  "/dev/shm/pushdown_res1",
+						  "/dev/shm/pushdown_res2",
 						  NULL };
 			pid_t pid = fork();
 			if (pid == 0) { // Child
@@ -236,10 +339,24 @@ static int pseudo_setxattr(const char *path, const char *name,
 		if (r != 0) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		int r = fsetxattr(state->res_fd, name, value, size, flags);
+		int r = fsetxattr(state->res_fd[0], name, value, size, flags);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fsetxattr(state->res_fd[1], name, value, size, flags);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = fsetxattr(state->res_fd[2], name, value, size, flags);
 		if (r != 0) {
 			return -1 * errno;
 		}
@@ -260,10 +377,24 @@ static int pseudo_getxattr(const char *path, const char *name, char *value,
 		if (r == -1) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		ssize_t r = fgetxattr(state->res_fd, name, value, size);
+		ssize_t r = fgetxattr(state->res_fd[0], name, value, size);
+		if (r == -1) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		ssize_t r = fgetxattr(state->res_fd[1], name, value, size);
+		if (r == -1) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		ssize_t r = fgetxattr(state->res_fd[2], name, value, size);
 		if (r == -1) {
 			return -1 * errno;
 		}
@@ -282,7 +413,9 @@ static int pseudo_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 		return -ENOENT;
 
 	filler(buf, "command", NULL, 0, 0);
-	filler(buf, "result", NULL, 0, 0);
+	filler(buf, "res0", NULL, 0, 0);
+	filler(buf, "res1", NULL, 0, 0);
+	filler(buf, "res2", NULL, 0, 0);
 
 	return 0;
 }
@@ -295,9 +428,19 @@ static void *pseudo_init(struct fuse_conn_info *conn, struct fuse_config *cfg)
 	if (command_fd == -1) {
 		exit(EXIT_FAILURE);
 	}
-	int res_fd =
-		open("/dev/shm/pushdown_res", O_RDWR | O_CREAT | O_TRUNC, 0666);
-	if (res_fd == -1) {
+	int res_fd0 = open("/dev/shm/pushdown_res0", O_RDWR | O_CREAT | O_TRUNC,
+			   0666);
+	if (res_fd0 == -1) {
+		exit(EXIT_FAILURE);
+	}
+	int res_fd1 = open("/dev/shm/pushdown_res1", O_RDWR | O_CREAT | O_TRUNC,
+			   0666);
+	if (res_fd1 == -1) {
+		exit(EXIT_FAILURE);
+	}
+	int res_fd2 = open("/dev/shm/pushdown_res2", O_RDWR | O_CREAT | O_TRUNC,
+			   0666);
+	if (res_fd2 == -1) {
 		exit(EXIT_FAILURE);
 	}
 	struct global_state *state =
@@ -306,7 +449,9 @@ static void *pseudo_init(struct fuse_conn_info *conn, struct fuse_config *cfg)
 		exit(EXIT_FAILURE);
 	} else {
 		state->command_fd = command_fd;
-		state->res_fd = res_fd;
+		state->res_fd[0] = res_fd0;
+		state->res_fd[1] = res_fd1;
+		state->res_fd[2] = res_fd2;
 	}
 	return state;
 }
@@ -315,7 +460,9 @@ static void pseudo_destroy(void *private_data)
 {
 	struct global_state *state = (struct global_state *)private_data;
 	close(state->command_fd);
-	close(state->res_fd);
+	close(state->res_fd[0]);
+	close(state->res_fd[1]);
+	close(state->res_fd[2]);
 	free(state);
 }
 
@@ -329,10 +476,24 @@ static int pseudo_utimens(const char *path, const struct timespec tv[2],
 		if (r != 0) {
 			return -1 * errno;
 		}
-	} else if (strcmp(path + 1, "result") == 0) {
+	} else if (strcmp(path + 1, "res0") == 0) {
 		struct global_state *state =
 			(struct global_state *)fuse_get_context()->private_data;
-		int r = futimens(state->res_fd, tv);
+		int r = futimens(state->res_fd[0], tv);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res1") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = futimens(state->res_fd[1], tv);
+		if (r != 0) {
+			return -1 * errno;
+		}
+	} else if (strcmp(path + 1, "res2") == 0) {
+		struct global_state *state =
+			(struct global_state *)fuse_get_context()->private_data;
+		int r = futimens(state->res_fd[2], tv);
 		if (r != 0) {
 			return -1 * errno;
 		}
